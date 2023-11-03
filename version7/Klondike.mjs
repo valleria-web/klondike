3e5b6b4627a2bb7a64c2ec6d96898f3fa4afa85f
vdsr <valleria@gmail.com>
2023-11-03 16:17:53 +0100

    foundationRules

--- version7/Klondike.mjs
+++ version7/Klondike.mjs
@@ -49,29 +49,4 @@ export default Klondike;
 //console.log(klondikeGameTeste.foundation.foundations);
 //console.log(klondikeGameTeste.tableau.tableaus);
 
-// Debug:
-const klondikeGameTeste = new Klondike();
-klondikeGameTeste.populateStockInKlondike();
-klondikeGameTeste.distributeCards();
-
-for (let i = 0; i < klondikeGameTeste.tableau.tableaus.length; i++) {
-  const testTableau = klondikeGameTeste.tableau.tableaus[i];
-  const cardToMove = testTableau[testTableau.length - 1];
-  console.log("Tableau:", i, testTableau);
-  console.log("Card to move:", cardToMove);
-
-  const foundationIndex = 0;
-  const isMoveToFoundationValid = klondikeGameTeste.isMoveToFoundationValid(cardToMove, foundationIndex);
-
-  if (isMoveToFoundationValid) {
-    const moveResult = klondikeGameTeste.moveCardToFoundation(cardToMove, foundationIndex);
-    if (moveResult) {
-      console.log('La carta se ha movido a la Foundation con éxito.');
-    } else {
-      console.log('La carta no se pudo mover a la Foundation.');
-    }
-  } else {
-    console.log('La carta no se puede mover a la Foundation según las reglas del juego.');
-  }
-}
 
